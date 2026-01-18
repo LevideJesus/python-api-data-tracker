fcbc9b6d3e3365634132285bfc102804f9c73994
Levi de Jesus <levijesus2121@gmail.com>
2026-01-18 12:01:20 -0300

    Added def menu to handle with the user´s menu

---
+++ main.py
@@ -0,0 +1,24 @@
+def menu():
+  print("=" * 50)
+  print("Welcome to the Weather Tracker!")
+  print("=" * 50)
+  print("Please choose any options below! ")
+  
+  print("1 - Fetch data from API")
+  print("2 - View saved data ")
+  print("3 - Exit")
+
+  choose = input("Your choose... ")
+
+  try:
+      if choose == "1":
+        pass
+      elif choose == "2":
+        pass
+      else:
+        pass
+  except
+
+print(menu())
+
+
